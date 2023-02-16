e08ecfe76c50e93fbf0763cf259b21e236068a09
Jack Terwilliger <jterwilliger30@gmail.com>
2023-02-16 15:02:47 -0800

    Angular V1.0
    This is not the best way to use VCS, but I took the functioning angular components from 'master' and added them to the functioning docker container in this branch, which seemed to work....

--- Azure-Webapp/src/app/app.component.ts
+++ Azure-Webapp/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Azure-Webapp';
+
+export class AppComponent implements OnInit {
+  title = 'msal-angular-tutorial';
+  isIframe = false;
+  loginDisplay = false;
+
+  constructor(private authService: MsalService, private http : HttpClient) { }
+
+  ngOnInit() {
+    this.isIframe = window !== window.parent && !window.opener;
+  }
+
+  options = {
+    headers: "",
+    observe: "response", // to display the full response
+    responseType: "json"
+  };
+
+  login() {
+    this.authService.loginPopup()
+      .subscribe({
+        next: (result) => {
+          console.log(result);
+          this.options.headers = result.accessToken;
+          this.setLoginDisplay();
+        },
+        error: (error) => console.log(error)
+      });
+  }
+
+  setLoginDisplay() {
+    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+  }
 }
