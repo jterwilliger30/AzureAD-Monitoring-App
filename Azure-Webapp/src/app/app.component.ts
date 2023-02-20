5371b8b7afb8e2930e0e2cb3a8510fa7e4b31bf7
Jack Terwilliger <jterwilliger30@gmail.com>
2023-02-20 12:38:50 -0800

    Implemented http.get request for User sign-in data

--- Azure-Webapp/src/app/app.component.ts
+++ Azure-Webapp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MsalService } from '@azure/msal-angular';
 
@@ -19,12 +19,28 @@ export class AppComponent implements OnInit {
     this.isIframe = window !== window.parent && !window.opener;
   }
 
+  ret_data : Object;
+
   login() {
     this.authService.loginPopup()
       .subscribe({
         next: (result) => {
           console.log(result);
           this.setLoginDisplay();
+
+          let headers = new HttpHeaders({
+            'Authorization': "Bearer " + result.accessToken,
+            'Content-Type': 'application/json'});
+          this.http.get("https://graph.microsoft.com/v1.0/auditLogs/signIns?&$filter=startsWith(userPrincipalName,'<INSERT USERID>')", { headers: headers }).subscribe(data => {
+            this.ret_data = data;
+            console.log(data);
+            var i
+            var x = ''
+            for (i in data['value']) {
+                x += "<p>" + i + "<span class=\"nums\">" + JSON.stringify(data['value'][i]) + "</span>" + "</p>"
+            }
+            document.getElementById('demo').innerHTML = x
+          });
         },
         error: (error) => console.log(error)
       });
